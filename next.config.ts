6699a6c8aca2b9fc12d459ab695d2006f540fdee
Harsh Sandhu <hsandhu@speareducation.com>
2026-01-20 13:30:22 +0530

    chore: add placeholder image url to next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.harshsandhu.com",
+        pathname: "/api/img",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
