9a6ed572649d0fb4894da6f2a7cbb71d1b551229
Harsh Sandhu <hsandhu@speareducation.com>
2026-01-25 20:45:07 +0530

    refactor: add loading and bell icon from lucide icons

--- components/pre-launch/email-form.tsx
+++ components/pre-launch/email-form.tsx
@@ -4,6 +4,7 @@ import { useActionState, useEffect, useRef } from "react";
 import { useFormStatus } from "react-dom";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { BellIcon, LoaderCircleIcon } from "lucide-react";
 
 interface EmailFormProps {
   action: (
@@ -26,30 +27,14 @@ function SubmitButton({ buttonText }: { buttonText: string }) {
     <Button type="submit" disabled={pending} className="min-w-30">
       {pending ? (
         <span className="flex items-center gap-2">
-          <svg
-            className="animate-spin h-4 w-4"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            ></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-          </svg>
+          <LoaderCircleIcon className="animate-spin h-4 w-4" />
           Saving...
         </span>
       ) : (
-        buttonText
+        <span className="flex items-center gap-2">
+          <BellIcon />
+          {buttonText}
+        </span>
       )}
     </Button>
   );
